c9f07bcd4f11cd1beb52d997fa154643ca067850
maria <mariia.logacheva99@gmail.com>
2024-12-09 17:01:14 +0300

    09.12

--- module_3_2.py
+++ module_3_2.py
@@ -1,21 +1,18 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
-
-    if  "@" not in sender and "@" not in recipient:
-
-        if sender.endswith(".com",".ru",".net"):
-            if not recipient.endswith(".com", ".ru", ".net"):
-
-             print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-
-    if sender == recipient:
+    if "@" not in recipient and "@" not in sender \
+            and not recipient.endswith((".com", ".ru", ".net")) \
+            and not sender.endswith((".com", ".ru", ".net")):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        return
+    if recipient == sender:
         print("Нельзя отправить письмо самому себе!")
         return
-    if sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса   {sender}   на адрес   {recipient}')
 
+    if sender == "university.help@gmail.com":
+        print(f'Письмо успешно отправлено с адреса {sender}   на адрес   {recipient}')
+        return
     else:
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса   {sender}   на адрес {recipient}')
-
 
 
 print(send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com'))
